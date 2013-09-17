90b726bdf179a7b0d815901786e489a14c7c6e79
Ramakrishnan Durairajan <ramakrish007@gmail.com>
2013-09-17 14:51:23 -0500

    Minor changes/comment addition

--- ii_2.py
+++ ii_2.py
@@ -1,5 +1,5 @@
 ## TODO
-## Add interpolation logic (best guess/guess in-roads)
-## Add physical information
-## Add peering information
+## Add interpolation logic (best guess/guess in-roads) - Graph Search? Seems like a good candidate
+## Add physical information - Internet Atlas
+## Add peering information - PeeringDB
 ## eat file from PhaseOne and apply other information
