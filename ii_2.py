5903a061b0f27adda014ab15b135f403e1949299
Ramakrishnan Durairajan <ramakrish007@gmail.com>
2013-09-30 11:17:41 -0500

    Algorithm for Phase two updated

--- ii_2.py
+++ ii_2.py
@@ -1,5 +1,15 @@
 ## TODO
-## Add interpolation logic (best guess/guess in-roads) - Graph Search? Seems like a good candidate
+## Higher level:
 ## Add physical information - Internet Atlas
 ## Add peering information - PeeringDB
 ## eat file from PhaseOne and apply other information
+##
+## Algorithm:
+## for every TR:
+##     search the info in neo4j for candidates
+##     found? Good
+##     Not found? Suggest Phase three targetted probing
+##
+## Lower level:
+## Import the following into neo4j
+## ASPaths, Physical locations, Peering
